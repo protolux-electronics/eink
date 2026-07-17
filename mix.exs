f370f6c50520fce41479ffbc22563abf4a5b8217
Frank Hunleth <fhunleth@troodon-software.com>
2026-07-17 14:48:39 -0400

    Bump Circuits.SPI to use one-way SPI w/ segmentation
    This provides a measurable performance improvement by avoiding the need
    to chunk the frame buffer in Elixir. In addition to getting rid of the
    binary chunks, it also gets rid of the read buffer allocations by
    ignoring the reads completely.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Eink.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_spi, "~> 2.0"},
+      {:circuits_spi, "~> 2.1"},
       {:circuits_gpio, "~> 2.0"},
       {:dither, "~> 0.2"}
     ]
