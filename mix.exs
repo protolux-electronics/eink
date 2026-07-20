852ee877a97f3a1de2b6b2945d4e07aae58711f6
Frank Hunleth <fhunleth@troodon-software.com>
2026-07-20 14:13:37 -0400

    Add ex_doc, credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -1,28 +1,84 @@
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
 defmodule Eink.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @source_url "https://github.com/protolux-electronics/eink"
 
   def project do
     [
       app: :eink,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs(),
+      dialyzer: dialyzer()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def cli do
+    [
+      preferred_envs: %{
+        docs: :docs,
+        "hex.publish": :docs,
+        "hex.build": :docs,
+        credo: :test
+      }
+    ]
+  end
+
   defp deps do
     [
       {:circuits_spi, "~> 2.1"},
-      {:circuits_gpio, "~> 2.0"}
+      {:circuits_gpio, "~> 2.0"},
+      {:credo, "~> 1.6", only: :test, runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.26", only: :docs, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Pure Elixir driver for EInk Panels"
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:missing_return, :extra_return, :unmatched_returns, :error_handling, :underspecs],
+      list_unused_filters: true,
+      plt_file: {:no_warn, "_build/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
